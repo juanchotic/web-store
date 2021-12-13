04765fd23efb18a0769782a571c343166794b504
juanchotic <jcatanohiguita@gmail.com>
2021-12-12 19:55:11 -0500

    se agregan cambios

--- app.js
+++ app.js
@@ -7,6 +7,36 @@ const templateCarrito = document.getElementById('template-carrito').content
 const fragment = document.createDocumentFragment()
 let carrito = {}
 
+// Method which actually read json using XMLHttpRequest and promise
+const jsonFileReader = async path => {
+    return new Promise((resolve, reject) => {
+
+        const request = new XMLHttpRequest();
+        request.open('GET', path, true);
+        request.responseType = 'blob';
+
+        request.onload = () => {
+          const reader = new FileReader();
+
+          reader.onload = e => resolve(e.target.result);
+          reader.onerror = err => reject(err);
+          reader.readAsDataURL(request.response);
+        };
+
+        request.send();
+    });
+}
+
+// This mthod will return the JSON
+const returnJsonData = async (url) => {
+    const jsonData = await jsonFileReader(url);
+    console.log('Here is your JSON data: => ', jsonData);
+    return jsonData;
+}
+
+// Calling the function where you put URL
+returnJsonData('api.json');
+
 // Eventos
 // El evento DOMContentLoaded es disparado cuando el documento HTML ha sido completamente cargado y parseado
 document.addEventListener('DOMContentLoaded', e => {
